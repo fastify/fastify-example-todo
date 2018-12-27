4e6309552482995a9125c9e06513d6cc354949ae
Furkan ILHAN <furkan7481@gmail.com>
2018-12-27 13:48:26 +0300

    Json schema 'getAll' fixed

--- schemas/todo.js
+++ schemas/todo.js
@@ -1,24 +1,22 @@
 'use strict'
 
-const getAll = {
-  response: {
-    200: {
-      type: 'array',
-      items: {
-        properties: {
+const getAll = { response: {
+  200: {
+    type: 'array',
+    items: {
+      properties: {
         // do not include _id field here so that it is removed from the response
         // _id: { type: 'string' },
-          name: { type: 'string' },
-          timestamp: { type: 'integer' },
-          done: { type: 'boolean' }
-        }
-      },
-      querystring: {
-        limit: { type: 'integer' },
-        offset: { type: 'integer' }
+        name: { type: 'string' },
+        timestamp: { type: 'integer' },
+        done: { type: 'boolean' }
       }
-    }
+    }    
   }
-}
+},
+querystring: {
+  limit: { type: 'integer' },
+  offset: { type: 'integer' }
+} }
 
 module.exports = { getAll }
