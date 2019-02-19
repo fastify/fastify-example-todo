5f21566de0c116c56a931c3cdebe42189cb3f4e9
Cemre Mengu <cemremengu@gmail.com>
2019-02-19 22:57:40 +0300

    Update timestamp.test.js

--- test/plugins/timestamp.test.js
+++ test/plugins/timestamp.test.js
@@ -1,14 +1,12 @@
 'use strict'
 
 const { test } = require('tap')
-const Fastify = require('fastify')
-const timestamp = require('../../plugins/timestamp')
+const { build } = require('../helper')
 
 test('support works standalone', async (t) => {
-  const fastify = Fastify()
-  fastify.register(timestamp)
-
+  const fastify = build(t)
   await fastify.ready()
+  
   const ts = fastify.timestamp()
   t.ok(new Date(ts) > 0)
 })
