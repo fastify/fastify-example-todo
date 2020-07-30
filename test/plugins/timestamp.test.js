e1deafd27c17f54a4d575b768f48b1100480438f
Sam Adams <adams.sam@gmail.com>
2020-07-30 10:53:18 +0100

    Fix broken tests (fastify-cors version and validation error messages) (#11)
    * fix tests: broken by latest fastify-cors
    
    * fix tests: invalid/outdated schema validation error messages

--- test/plugins/timestamp.test.js
+++ test/plugins/timestamp.test.js
@@ -6,7 +6,7 @@ const { build } = require('../helper')
 test('support works standalone', async (t) => {
   const fastify = build(t)
   await fastify.ready()
-  
+
   const ts = fastify.timestamp()
   t.ok(new Date(ts) > 0)
 })
